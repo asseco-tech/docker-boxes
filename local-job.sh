5095e6e4586bf632c99005f94835a17cdb11cedc
Mariusz Jasinski <Mariusz.Jasinski@asseco.pl>
2025-12-08 18:07:06 +0100

    ubi8-test

--- local-job.sh
+++ local-job.sh
@@ -2,13 +2,18 @@
 
 REGISTRY=${REGISTRY:-artifactory-test.dev-pus.asseco.pl/com-image-snapshot}
 BUILD_NUMBER=${BUILD_NUMBER:-001}
+CACHE=1
 
-(cd kdevbox8 \
-    && make build BUILD_NUMBER=${BUILD_NUMBER} \
-    && make push DOCKER_REGISTRY=${REGISTRY} BUILD_NUMBER=${BUILD_NUMBER} \
-    && make clean BUILD_NUMBER=${BUILD_NUMBER} )
+IMAGE_DIRS=(
+  'ubi8-test'
+  'ubi9-test'
+  'kdevbox8'
+  'kubebox8'
+)
 
-(cd kubebox8 \
-    && make build BUILD_NUMBER=${BUILD_NUMBER} \
-    && make push DOCKER_REGISTRY=${REGISTRY} BUILD_NUMBER=${BUILD_NUMBER} \
-    && make clean BUILD_NUMBER=${BUILD_NUMBER} )
+for idir in "${IMAGE_DIRS[@]}"; do
+  (cd ${idir} \
+      && make build BUILD_NUMBER=${BUILD_NUMBER} CACHE=${CACHE} \
+      && make push DOCKER_REGISTRY=${REGISTRY} BUILD_NUMBER=${BUILD_NUMBER} \
+      && make clean BUILD_NUMBER=${BUILD_NUMBER} )
+done
