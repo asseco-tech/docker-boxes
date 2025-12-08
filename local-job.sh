1d8291b90f9f689245539c9ae5cb11a953de083f
Mariusz Jasinski <Mariusz.Jasinski@asseco.pl>
2025-12-08 17:12:45 +0100

    make clean

--- local-job.sh
+++ local-job.sh
@@ -3,10 +3,12 @@
 REGISTRY=artifactory-test.dev-pus.asseco.pl/com-image-snapshot
 BUILD_NUMBER=001
 
-(cd kubebox8 \
+(cd kdevbox8 \
     && make build BUILD_NUMBER=${BUILD_NUMBER} \
-    && make push DOCKER_REGISTRY=${REGISTRY} BUILD_NUMBER=${BUILD_NUMBER} )
+    && make push DOCKER_REGISTRY=${REGISTRY} BUILD_NUMBER=${BUILD_NUMBER} \
+    && make clean BUILD_NUMBER=${BUILD_NUMBER} )
 
 (cd kubebox8 \
     && make build BUILD_NUMBER=${BUILD_NUMBER} \
-    && make push DOCKER_REGISTRY=${REGISTRY} BUILD_NUMBER=${BUILD_NUMBER} )
+    && make push DOCKER_REGISTRY=${REGISTRY} BUILD_NUMBER=${BUILD_NUMBER} \
+    && make clean BUILD_NUMBER=${BUILD_NUMBER} )
